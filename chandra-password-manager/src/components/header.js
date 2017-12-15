cdfebe7c60e0f3907d7bd9586d5879780fbbd734
chandrabuwana <cbuwana@gmail.com>
2017-12-15 18:06:39 +0700

    router link

---
+++ chandra-password-manager/src/components/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Formjs from './Header/form'
+class Header extends Component{
+    render(){
+        return (
+            <div>
+                
+                <Formjs />
+            </div>
+        )
+    }
+}
+
+export default Header 
