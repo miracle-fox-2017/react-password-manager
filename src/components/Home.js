e795131986acfee7dce81ee865c53d509e074284
Faris Widyantho <radenfaris@gmail.com>
2017-12-16 21:03:30 +0700

    add feature search, createdAt, updatedAt

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,30 +11,65 @@ class Home extends Component {
   constructor(props) {
     super()
     this.state = {
-      url: '',
-      username: '',
-      password: ''
+      allPassword: [],
+      url: ''
     }
     this.submitHandler = this.submitHandler.bind(this)
+    this.clearUrl = this.clearUrl.bind(this)
   }
 
-  submitHandler() {
-    this.props.addPassword(this.state)
+  submitHandler(url) {
+    let newManipulator = this.state.allPassword.filter(password => {
+      return password.url === this.state.url
+    })
     this.setState({
       url: '',
-      username: '',
-      password: ''
+      allPassword: newManipulator
     })
   }
 
   deleteHandler(item) {
+    let newArrs = this.state.allPassword.filter(function(pass) {
+      return pass.id !== item.id
+    })
+    this.setState({
+      url: '',
+      allPassword: newArrs
+    })
     this.props.removePassword(item)
+  }
+
+  clearUrl(){
+    this.setState({
+      url: '',
+      allPassword: this.props.getPassword
+    })
+  }
+
+  componentWillMount(){
+    this.setState({
+      allPassword: this.props.getPassword
+    })
   }
 
   render() {
     return (
       <div className="container" style={containerFix}>
-        <div className="column is-half is-offset-one-quarter">
+        <div className="column">
+          <div className="field">
+            <label className="label">Find by URL</label>
+          </div>
+          <div className="field has-addons">
+            <div className="control">
+              <input value={this.state.url} className="input" type="text" placeholder="http://hacktiv8.com" onChange={ (e) => this.setState({ url: e.target.value }) } />
+            </div>
+            <div className="control">
+              <button onClick={this.submitHandler} className="button is-info"><i className="fa fa-search" aria-hidden="true"></i></button>
+              <button onClick={this.clearUrl} className="button is-white"><i className="fa fa-refresh" aria-hidden="true"></i></button>
+            </div>
+          </div>
+        </div>
+        <div className="column">
           <table className="table is-hoverable" style={{textAlign: 'center'}}>
             <thead>
               <tr>
@@ -42,17 +77,21 @@ class Home extends Component {
                 <th>Url</th>
                 <th>Username</th>
                 <th>Password</th>
+                <th>Created At</th>
+                <th>Updated At</th>
                 <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              { this.props.getPassword.map((password, i) => {
+              { this.state.allPassword.map((password, i) => {
                 return (
                   <tr key={password.id}>
                     <th>{i+1}</th>
                     <td>{password.url}</td>
                     <td>{password.username}</td>
                     <td>{password.password}</td>
+                    <td>{password.createdAt.toString()}</td>
+                    <td>{password.updatedAt.toString()}</td>
                     <td>
                       <Link to={"/edit/"+password.id} className="button is-warning is-small">
                         Edit
--- src/reducers/passwordManager.js
+++ src/reducers/passwordManager.js
@@ -3,12 +3,16 @@ const initialState = {
     id: "1",
     url: "helooooo",
     username: "asdasda",
-    password: "asdasda"
+    password: "asdasda",
+    createdAt: new Date(),
+    updatedAt: ''
   },{
     id: "2",
     url: "heloooooxxx",
     username: "asdasdaasda",
-    password: "asdasdadsasda"
+    password: "asdasdadsasda",
+    createdAt: new Date(),
+    updatedAt: ''
   }]
 }
 
@@ -16,6 +20,8 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PASSWORD':
       action.payload.id = (state.passwordStore.length + 1).toString()
+      action.payload.createdAt = new Date()
+      action.payload.updatedAt = ''
       let newArr = state.passwordStore.concat(action.payload)
       return {...state, passwordStore: newArr}
     case 'REMOVE_PASSWORD':
@@ -24,8 +30,10 @@ const reducer = (state = initialState, action) => {
       })
       return {...state, passwordStore: newArrs}
     case 'EDIT_PASSWORD':
-      let newPassword = state.passwordStore.map(pass => {
+      let newPassword = state.passwordStore.map((pass, i) => {
         if(pass.id === action.payload.id){
+          action.payload.createdAt = state.passwordStore[i].createdAt
+          action.payload.updatedAt = new Date()
           return action.payload
         }
         return pass
