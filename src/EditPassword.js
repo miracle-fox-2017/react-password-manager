54104804d1946787ad41d6b859f350002c1361f1
wisnudj <jayalahwisnu@gmail.com>
2017-12-17 23:37:35 +0700

    penambahan validasi di edit

--- src/EditPassword.js
+++ src/EditPassword.js
@@ -14,7 +14,12 @@ class EditPassword extends Component {
         URL: '',
         username: '',
         password: '',
-      }
+      },
+      visiblehurufbesar: false,
+      visiblehurufkecil: false,
+      visibleangka: false,
+      visiblespecialchar: false,
+      visiblelength: false
     }
     console.log('ini dari edit', props.match.params.id)
   }
@@ -36,16 +41,90 @@ class EditPassword extends Component {
 
   handleUpdate =(e) => {
     e.preventDefault()
-    this.props.updatepassword(this.state.newpass, this.props.match.params.id)
-    this.props.history.push('/')
-    let newpass = Object.assign({}, this.state.newpass);
-    newpass.URL = ''
-    newpass.username = ''
-    newpass.password = ''
-    this.setState({newpass})
+
+    var besar = true
+    var kecil = true
+    var spesial = true
+    var angka = true
+    var panjang = true
+
+    if(!(/[A-Z]/.test(this.state.newpass.password))) {
+      this.setState({
+        visiblehurufbesar: true
+      })
+      besar = false
+      console.log('besar:', besar);
+    }
+
+    if(!(/[a-z]/.test(this.state.newpass.password))) {
+      this.setState({
+        visiblehurufkecil: true
+      })
+
+      kecil = false
+      console.log('kecil:',kecil)
+    }
+
+    if(!(/[0-9]/.test(this.state.newpass.password))) {
+      this.setState({
+        visibleangka: true
+      })
+
+      angka = false
+      console.log('angka:', angka)
+    }
+
+    if(!(/[#$@!&%]/.test(this.state.newpass.password))) {
+      this.setState({
+        visiblespecialchar: true
+      })
+
+      spesial = false
+      console.log('spesial:', spesial)
+    }
+
+    if(!(/[a-zA-Z0-9#$@!&%]{5}$/.test(this.state.newpass.password))) {
+      this.setState({
+        visiblelength: true
+      })
+
+      panjang = false
+      console.log('panjang:', panjang)
+    }
+
+    if(besar && kecil && spesial && angka && panjang) {
+      this.props.updatepassword(this.state.newpass, this.props.match.params.id)
+      this.setState({
+        visiblehurufbesar: false,
+        visiblehurufkecil: false,
+        visibleangka: false,
+        visiblespecialchar: false,
+        visiblelength: false
+      })
+      this.props.history.push('/')
+    }
   }
 
   render() {
+    if(this.state.visiblehurufbesar) {
+      var pesanbesar = <p id="emailHelp" class="form-text text-muted">Password harus memiliki setidaknya 1 huruf besar</p>
+    }
+
+    if(this.state.visiblehurufkecil) {
+      var pesankecil = <p id="emailHelp" class="form-text text-muted">Password harus memiliki setidaknya 1 huruf kecil</p>
+    }
+
+    if(this.state.visibleangka) {
+      var pesanangka = <p id="emailHelp" class="form-text text-muted">Password harus memiliki setidaknya 1 angka</p>
+    }
+
+    if(this.state.visiblespecialchar) {
+      var pesanspesial = <p id="emailHelp" class="form-text text-muted">Password harus memiliki setidaknya 1 spesial karakter(#$@!&%)</p>
+    }
+
+    if(this.state.visiblelength) {
+      var pesanlength = <p id="emailHelp" class="form-text text-muted">Password harus minimal 5 karakter</p>
+    }
     return (
         <div className="container">
         <form onSubmit={this.handleUpdate}>
@@ -61,11 +140,16 @@ class EditPassword extends Component {
             </div>
             <div className="form-group">
               <label htmlFor="passwordkunci">Password</label>
-              <input type="password" className="form-control" id="passwordkunci" placeholder="Password" name="password" defaultValue={this.state.newpass.password} onChange={this.handleChange}/>
+              <input type="text" className="form-control" id="passwordkunci" placeholder="Password" name="password" defaultValue={this.state.newpass.password} onChange={this.handleChange}/>
             </div>
             <button type="submit" className="btn btn-primary">Update</button>
           </fieldset>
         </form>
+        {pesanbesar}
+        {pesankecil}
+        {pesanangka}
+        {pesanspesial}
+        {pesanlength}
       </div>
     )
   }
